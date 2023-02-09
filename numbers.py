1b9772010328df307371f30e4e9848cf6739a891
Jackson Hardy <jacksonhardy90@gmail.com>
2023-02-08 18:49:12 -0700

    random number

---
+++ numbers.py
@@ -0,0 +1,16 @@
+# python numbers
+import random 
+
+newNum = 0
+aNumber = 10 #created datatype int
+floatNumber = 2.6
+
+print (type(aNumber))
+aNumber = float(aNumber)
+
+print (aNumber)
+print (aNumber + floatNumber)
+
+newNum = random.randrange(12, 80)
+
+print (newNum) 
